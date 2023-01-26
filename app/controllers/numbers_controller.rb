b599268e2ee9983d2c5080650a923c5a877eab5c
rgillespie23 <rgillespie@uchicago.edu>
2023-01-26 23:36:21 +0000

    Done with Part 1

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,17 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @owl = Array.new
+
+    5.times do
+      rat = rand(1...100)
+
+      @owl.push(rat)
+    end
+
+  render({ :template => "lottery_stuff/pow.html.erb"})
+  end
+  
 end
